973b6c70e1a28808d92696f504096bae926c7abd
omarse7a <omar.seha90@gmail.com>
2024-12-15 21:31:59 +0200

    added course model

---
+++ src/main/java/com/dev/LMS/model/Course.java
@@ -0,0 +1,94 @@
+package com.dev.LMS.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.Objects;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique=true, nullable=false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private Float duration;    // completion hours
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.SET_NULL)
+    private User instructor;
+
+    public Course() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+       this.id = id ;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Float duration) {
+        this.duration = duration;
+    }
+
+    public User getInstructor_id() {
+        return instructor;
+    }
+
+    public void setInstructor_id(User instructor_id) {
+        this.instructor = instructor_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id && Objects.equals(name, course.name) && Objects.equals(description, course.description) && Objects.equals(duration, course.duration) && Objects.equals(instructor, course.instructor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, duration, instructor);
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", duration=" + duration +
+                ", instructor_id=" + instructor +
+                '}';
+    }
+}
