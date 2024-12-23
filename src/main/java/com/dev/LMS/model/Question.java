f653ecdd9d2df46438d6a7e17734fe07b95862db
mr-70da <man3092003city@gmail.com>
2024-12-23 22:59:31 +0200

    Fixed: sum issuses

--- src/main/java/com/dev/LMS/model/Question.java
+++ src/main/java/com/dev/LMS/model/Question.java
@@ -137,4 +137,8 @@ public class Question {
                 ", course=" + course +
                 '}';
     }
+
+    public void addSubmission(QuizSubmission quizSubmission) {
+        this.submissions.add(quizSubmission);
+    }
 }
